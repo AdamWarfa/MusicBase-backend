0cb942244f18cb201697aa09e8935026bd4a3bfb
yousradiab <yousserah@gmail.com>
2023-09-21 11:47:52 +0200

    disconnect

--- database.js
+++ database.js
@@ -1,18 +1,16 @@
 import mysql from "mysql2";
-import dotenv from "dotenv";
+import dotenv from "dotenv/config";
 import fs from "fs";
 
-dotenv.config();
-
 const dbconn = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   port: 3306,
 };
 if (process.env.MYSQL_CERT) {
-  dbconn.ssl = { cs: fs.readFileSync("DigiCertGlobalRootCA.crt.pem") };
+  dbconn.ssl = { ca: fs.readFileSync("DigiCertGlobalRootCA.crt.pem") };
 }
 
 const db = mysql.createConnection(dbconn);
